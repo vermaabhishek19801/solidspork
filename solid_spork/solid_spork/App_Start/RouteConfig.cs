a8bc8e96655c3bc4bf12502c18141dd57e98a6ab
Deepak Mishra <mail.deepak899@gmail.com>
2016-04-24 21:56:27 +0530

    Converted to MVC c#

---
+++ solid_spork/solid_spork/App_Start/RouteConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace solid_spork
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.MapRoute(
+               name: "Default",
+               url: "{controller}/{action}/{id}",
+               defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+           );
+            routes.MapRoute(
+                name: "Query",
+                url: "{controller}/{action}/{id}/{details}",
+                defaults: new { controller = "Query", action = "QueryDetails", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ solid_spork/solid_spork/Controllers/queryController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace solid_spork.Controllers
+{
+    public class queryController : Controller
+    {
+        //
+        // GET: /query/
+
+        public ActionResult Query()
+        {
+            return View();
+        }
+
+        public ActionResult QueryDetails(int id, string details)
+        {
+            ViewBag.Title = "solid-work.com " + details ?? "";          
+            return View();
+        }
+
+
+    }
+}
